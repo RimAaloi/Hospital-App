e601571e400bad7694d7d93f15a42fb58eae5df4
RimAaloi <aaloirim@example.com>
2025-03-17 16:37:42 +0100

    add class controller

---
+++ src/main/java/ma/enset/hospitalapp/web/PatientController.java
@@ -0,0 +1,30 @@
+package ma.enset.hospitalapp.web;
+
+import ma.enset.hospitalapp.entities.Patient;
+import ma.enset.hospitalapp.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PatientController {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @GetMapping({"/index"})
+    public String getpatient(Model model,
+                             @RequestParam(name ="name",defaultValue = "0")int page,
+                             @RequestParam(name = "size",defaultValue = "4")int size) {
+        Page<Patient> pagepatients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listpatients", pagepatients.getContent());
+        model.addAttribute("pages",new int[pagepatients.getTotalPages()]);
+
+        return "patients";
+    }
+
+}
